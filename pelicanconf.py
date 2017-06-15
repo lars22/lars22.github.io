e0e660ca4fccbdf306b9d714af4ac75f9ae99272
lars22 <larsweitendorf@Larss-Air.fritz.box>
2017-06-15 11:38:36 +0200

    website

---
+++ pelicanconf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'lars'
+SITENAME = u'lars'
+SITEURL = 'http://lars22.github.io'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = u'de'
+THEME = 'medius-master'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('500px', 'http://500px.com/lars22'),
+         ('Instagram', 'http://instagram.com/lars90w'),
+         ('Impressum', '#'),)
+
+# Social widget
+SOCIAL = (('500px', 'http://500px.com/lars22'),
+          ('Instagram', 'http://instagram.com/lars90w'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
